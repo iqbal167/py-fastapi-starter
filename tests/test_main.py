0ccadea9d2946776f22dc8309b64b0640dda5d3a
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2025-07-28 11:17:55 +0700

    test: update expected response in test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"message": "Hello World"}
